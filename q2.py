ebcd2d9514e93ef02fa74e995475218151f2203a
Anton Yabchinskiy <arn@bestmx.net>
2017-01-24 16:57:09 +0300

    q2: wip

--- q2.py
+++ q2.py
@@ -18,7 +18,11 @@ def cat(n):
             c[i] =  c[i] + c[k - 1] * c[i - k]
     return c[n]
 
-comp = {'A': 'U', 'C': 'G', 'G': 'C', 'U': 'A'}
+# comp = {'A': 'U', 'C': 'G', 'G': 'C', 'U': 'A'}
+comp = {ord(b'A'): ord(b'U'),
+        ord(b'C'): ord(b'G'),
+        ord(b'G'): ord(b'C'),
+        ord(b'U'): ord(b'A')}
 
 def findall(s, sub, start=0):
     while True:
@@ -29,15 +33,9 @@ def findall(s, sub, start=0):
         else:
             break
 
-def maybeperfect(s, start, end):
-    a = s.count('A', start, end)
-    c = s.count('C', start, end)
-    g = s.count('G', start, end)
-    u = s.count('U', start, end)
-    debug('maybeperfect: "%s" %d %d %d %d', s[start:end], a, c, g, u)
-    return a == u and c == g
-
-s = stdin.readline().strip().upper()
+# s = stdin.readline().strip().upper()
+s = bytearray(stdin.readline().strip().upper(), 'ascii')
+debug('s = "%s"', s)
 # t = {nt: list(findall(s, nt)) for nt in 'ACGU'}
 # debug('t = %s', t)
 
@@ -59,60 +57,124 @@ s = stdin.readline().strip().upper()
 #             return True
 #     return False
 
-bonds = dict()
-singles = set()
+# bonds = dict()
+# singles = set()
+#
+# def isperfect(s, start, end, level=0):
+#     n = end - start
+#     debug('%ss = "%s", start = %d, end = %d, n = %s',
+#           ' ' * level, s[start:end], start, end, n)
+#     if n == 0:
+#         return True
+#     if n == 1:
+#         singles.add(start)
+#         return True
+#     if n == 2:
+#         if s[start] == comp[s[start + 1]]:
+#             bonds[start] = start + 1
+#             debug('bond (%d, %d)', start, start + 1)
+#             return True
+#         else:
+#             return False
+#     for i in range(start + 1, end): # +2
+#         if s[i] == comp[s[start]]:
+#             if isperfect(s, start + 1, i, level + 1) and isperfect(s, i + 1, end, level + 1):
+#                 bonds[start] = i
+#                 debug('bond (%d, %d)', start, i)
+#                 return True
+#     return False
 
-def isperfect(s, start, end, level=0):
-    n = end - start
-    debug('%ss = "%s", start = %d, end = %d, n = %s',
-          ' ' * level, s[start:end], start, end, n)
-    if n == 0:
+# ok = isperfect(s, 0, len(s))
+# debug('bonds = %s', bonds)
+# debug('singles = %s', singles)
+# if ok:
+#     if len(s) % 2 == 0:
+#         if len(singles) == 0:
+#             print('perfect')
+#         else:
+#             print('imperfect')
+#     else:
+#         if len(singles) > 0:
+#             print('almost perfect')
+#         else:
+#             print('imperfect')
+# else:
+#     print('imperfect')
+
+#-----------------------------------------------------------------------------
+
+def counts(s, start=0, end=None):
+    if isinstance(s, bytearray):
+        return (s.count(b'A', start, end),
+                s.count(b'C', start, end),
+                s.count(b'G', start, end),
+                s.count(b'U', start, end))
+    else:
+        return (s.count('A', start, end),
+                s.count('C', start, end),
+                s.count('G', start, end),
+                s.count('U', start, end))
+
+def maybeperfect(s, start=0, end=None):
+    a, c, g, u = counts(s, start, end)
+    debug('maybeperfect: "%s" %d %d %d %d', s[start:end], a, c, g, u)
+    return a == u and c == g
+
+def isperfect2(s, start, end):
+    if (end - start) == 0:
         return True
-    if n == 1:
-        singles.add(start)
-        return True
-    if n == 2:
-        if s[start] == comp[s[start + 1]]:
-            bonds[start] = start + 1
-            debug('bond (%d, %d)', start, start + 1)
-            return True
-        else:
-            return False
     if not maybeperfect(s, start, end):
         return False
-    for i in range(start + 1, end): # +2
+    for i in range(start + 1, end):
         if s[i] == comp[s[start]]:
-            if isperfect(s, start + 1, i, level + 1) and isperfect(s, i + 1, end, level + 1):
-                bonds[start] = i
+            if isperfect2(s, start + 1, i) and isperfect2(s, i + 1, end):
                 debug('bond (%d, %d)', start, i)
                 return True
     return False
 
-ok = isperfect(s, 0, len(s))
-debug('bonds = %s', bonds)
-debug('singles = %s', singles)
-if ok:
-    if len(s) % 2 == 0:
-        if len(singles) == 0:
-            print('perfect')
+tryremove = None
+
+if len(s) % 2 == 0:
+    if isperfect2(s, 0, len(s)):
+        print('perfect')
+    else:
+        print('imperfect')
+else:
+    a, c, g, u = counts(s)
+    debug('%d %d %d %d', a, c, g, u)
+    if a == u:
+        if c - g == 1:
+            # Try remove C
+            tryremove = b'C'
+        elif g - c == 1:
+            # Try remove G
+            tryremove = b'G'
+        else:
+            print('imperfect')
+    elif c == g:
+        if a - u == 1:
+            # Try remove A
+            tryremove = b'A'
+        elif u - a == 1:
+            # Try remove U
+            tryremove = b'U'
         else:
             print('imperfect')
     else:
-        if len(singles) > 0:
-            print('almost perfect')
-        else:
-            print('imperfect')
-else:
-    print('imperfect')
+        print('imperfect')
 
-# s = stdin.readline().strip()
-# debug(s)
-#
-# basepair = dict()
-# for i in range(len(s) - 2):
-#     e = set(findall(s, comp(s[i]), i + 2))
-#     if e:
-#         basepair[i] = e
-# debug(basepair)
 
-# TODO
+if tryremove:
+    found = False
+    debug('tryremove %s', tryremove)
+    for pos in findall(s, tryremove):
+        debug('pos = %s', pos)
+        s[pos], s[0] = s[0], s[pos]
+        if isperfect2(s, 1, len(s)):
+            found = True
+            break
+        s[pos], s[0] = s[0], s[pos]
+    if found:
+        print('almost perfect')
+    else:
+        print('imperfect')
