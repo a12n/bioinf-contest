0a6e89f907116b4f28af01e733c69ffaf3c1d570
Anton Yabchinskiy <arn@bestmx.net>
2017-02-18 05:43:17 +0300

    f1: IntervalSet class

--- f1.py
+++ f1.py
@@ -2,39 +2,41 @@
 
 from sys import stdin
 
-def intervals(l):
-    return [(l[2 * i], l[2 * i + 1]) for i in range(len(l) // 2)]
+class IntervalSet:
+    def _intersects(a, b):
+        return a[0] <= b[1] and b[0] <= a[1]
+
+    def __init__(self, l):
+        # self.first = [l[2 * i] for i in range(len(l) // 2)]
+        # self.last = [l[2 * i + 1] for i in range(len(l) // 2)]
+        # self.first.sort()
+        # self.last.sort()
+        self.intervals = [(l[2 * i], l[2 * i + 1]) for i in range(len(l) // 2)]
+
+    def intersects(self, other):
+        for i in self.intervals:
+            for j in other.intervals:
+                if IntervalSet._intersects(i, j):
+                    return True
+        return False
 
 n, m = map(int, stdin.readline().split())
-genes = [intervals(list(map(int, stdin.readline().split()))) for _ in range(n)]
-reads = [intervals(list(map(int, stdin.readline().split()))) for _ in range(m)]
-# print('genes', genes)
-# print('reads', reads)
-
-def intersects0(a, b):
-    return a[0] <= b[1] and b[0] <= a[1]
-
-def intersects(gene, read):
-    for g in gene:
-        for r in read:
-            if intersects0(r, g):
-                # print('gene', gene, 'read', read, True)
-                return True
-    # print('gene', gene, 'read', read, False)
-    return False
-
-intersects_tab = [[False] * n for _ in range(m)]
-for i in range(len(genes)):
-    for j in range(len(reads)):
-        intersects_tab[j][i] = intersects(genes[i], reads[j])
-# print(intersects_tab)
+genes = [IntervalSet(list(map(int, stdin.readline().split()))) for _ in range(n)]
+reads = [IntervalSet(list(map(int, stdin.readline().split()))) for _ in range(m)]
 
 ans = [0] * n
+intersects = [[False] * n for _ in range(m)]
+
+for j in range(len(reads)):
+    for i in range(len(genes)):
+        intersects[j][i] = genes[i].intersects(reads[j])
+# print(intersects)
+
 for j in range(len(reads)):
     try:
-        i = intersects_tab[j].index(True, 0)
+        i = intersects[j].index(True, 0)
         try:
-            intersects_tab[j].index(True, i + 1)
+            intersects[j].index(True, i + 1)
         except ValueError:
             ans[i] += 1
     except ValueError:
