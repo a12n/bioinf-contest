1565da051260f1816daa0bd217a103a9efe709cf
Anton Yabchinskiy <arn@bestmx.net>
2017-02-18 12:42:43 +0300

    f2: initial solution

--- f2.py
+++ f2.py
@@ -43,7 +43,30 @@ def reconstruct():
     origkmers = set((stdin.readline().rstrip() for _ in range(n)))
     assert all(len(kmer) == k for kmer in origkmers)
     state = list(origkmers)
+    def mergekmers():
+        for i in range(len(state)):
+            for j in range(len(state)):
+                # TODO: try different overlap lenghts
+                s = overlap(state[i], state[j])
+                skmers = kmers_set(s, k)
+                if not skmers.issubset(origkmers):
+                    continue
+                if any(not skmers.isdisjoint(kmers_set(state[m], k)) \
+                       for m in range(len(state)) if m != i and m != j):
+                    continue
+                return (s, state[i], state[j])
+        return None
     # TODO
+    stop = False
+    while len(state) > 1 and not stop:
+        res = mergekmers()
+        if res:
+            s, i, j = res
+            state.remove(i)
+            state.remove(j)
+            state.insert(0, s)
+        else:
+            stop = True
     print(len(state))
     print('\n'.join(state))
 
