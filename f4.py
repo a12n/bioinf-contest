01f8be8f57f7021638ca81e68fc7206b68d9bd8f
Anton Yabchinskiy <arn@bestmx.net>
2017-02-18 09:30:14 +0300

    f4: WIP, using GC-content

---
+++ f4.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+from sys import stdin
+
+import numpy
+import matplotlib.pyplot as plot
+
+def gccontent(s):
+    return (s.count('C') + s.count('G')) / len(s)
+
+_ = stdin.readline()
+s = ''.join(map(str.rstrip, stdin))
+assert sum([s.count(c) for c in 'ACGT']) == len(s)
+
+def parts(s, n):
+    i = 0
+    while True:
+        t = s[(i * n):((i + 1) * n)]
+        if not t:
+            break
+        yield t
+        i += 1
+
+# print(s)
+# print(gccontent(s))
+# print(list(map(gccontent, parts(s, 10))))
+
+k = 50
+subs = list(parts(s, k))
+mean = gccontent(s)
+std = numpy.std(list(map(gccontent, subs)))
+print('mean', mean, 'std', std)
+
+def weirdgc(subs, mean, std, alpha=2.2):
+    for i in range(len(subs)):
+        gc = gccontent(subs[i])
+        if abs(gc - mean) > (alpha * std):
+            yield (i, gc)
+
+weird = list(weirdgc(subs, mean, std))
+
+for i, gc in weird:
+    print('Part at', k * i, 'GC content', gc, 'is weird')
+
+first, _ = weird[0]
+last, _ = weird[-1]
+
+print('first', k * first, 'last', k * last)
+
+# plot.plot(list(map(gccontent, parts(s, len(s) // 1000))))
+# plot.show()
+
+# for t in parts(s, 100):
+#     print(t[:15], gccontent(t))
