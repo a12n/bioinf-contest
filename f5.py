ea5a725e0d4a14b9859f85cdafbc5907b57210cd
Anton Yabchinskiy <arn@bestmx.net>
2017-02-18 18:15:35 +0300

    f5: initial source file

---
+++ f5.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import logging
+import sys
+
+logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+def parsechems(s, sep=None):
+    return set(map(str.strip, s.split(sep)))
+
+def parsereaction(s):
+    return tuple(map(lambda t: parsechems(t, '+'), s.split('<=>')))
+
+with open(sys.argv[1], 'r') as f:
+    reactions = list(map(parsereaction, f))
+
+with open(sys.argv[2], 'r') as f:
+    proteins = parsechems(next(f))
+
+logging.debug('%d reactions %s', len(reactions), reactions)
+logging.debug('%d proteins %s', len(proteins), proteins)
