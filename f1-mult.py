819b32aba0acca62d2d082808a27a1e92990777c
Anton Yabchinskiy <arn@bestmx.net>
2017-03-20 22:31:37 +0300

    f1-mult: keep WIP changes

--- f1-mult.py
+++ f1-mult.py
@@ -4,47 +4,65 @@ from bisect import bisect_left, bisect_right
 from sys import stdin
 import logging
 
-logging.basicConfig(format='%(message)s', level=logging.INFO)
+logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
 def intersects(a, b):
     return a[0] <= b[1] and b[0] <= a[1]
 
 class DIS2:
     def __init__(self, ivlists):
-        tmp = []
+        self.mapping = dict()
+
+        self.first = []
+        self.last = []
         for i in range(len(ivlists)):
             for iv in ivlists[i]:
-                tmp.append((iv[0], iv[1], i))
-        tmp.sort(key=lambda e: e[0])
-        self.first = [iv[0] for iv in tmp]
-        self.last = [iv[1] for iv in tmp]
-        self.index = [iv[2] for iv in tmp]
-        logging.debug('first %s', self.first)
-        logging.debug('last %s', self.last)
-        logging.debug('index %s', self.index)
+                self.first.append(iv)
+                self.last.append(iv)
+                self.mapping[iv] = i
+        self.first.sort(key=lambda t: t[0])
+        self.last.sort(key=lambda t: t[1])
+
+        self.first_last = [t[1] for t in self.first]
+        # self.first_index = [t[2] for t in self.first]
+        self.first = [t[0] for t in self.first]
+
+        self.last_first = [t[0] for t in self.last]
+        # self.last_index = [t[2] for t in self.last]
+        self.last = [t[1] for t in self.last]
+
+        logging.debug('first %s %s', self.first, self.first_last)
+        logging.debug('last %s %s', self.last_first, self.last)
+
+    def __len__(self):
+        return len(self.first)
 
     def _intersects1(self, iv):
         i = bisect_right(self.first, iv[1])
         logging.debug('first %s, iv[1] %s, i %s', self.first, iv[1], i)
         if i == 0:
-            return []
-        if not intersects((self.first[i - 1], self.last[i - 1]), iv):
-            return []
+            return 0
+        if not intersects((self.first[i - 1], self.first_last[i - 1]), iv):
+            logging.debug('no intersection %s %s', (self.first[i - 1], self.first_last[i - 1]), iv)
+            return 0
         j = bisect_left(self.last, iv[0])
         logging.debug('last %s, iv[0] %s, j %s', self.last, iv[0], j)
         if j == len(self.last):
-            return []
-        if not intersects((self.first[j], self.last[j]), iv):
-            return []
-        logging.debug('index[%d:%d] %s', j, i, self.index[j:i])
-        return self.index[j:i]
+            return 0
+        if not intersects((self.last_first[j], self.last[j]), iv):
+            logging.debug('no intersection %s %s', (self.last_first[j], self.last[j]), iv)
+            return 0
+        logging.debug('i %d, j %d = %d', i, j, i - j)
+        # TODO
+        return i - j
 
     def intersects(self, ivs):
         logging.debug('ivs %s', ivs)
-        ans = set()
+        ans = 0
         for iv in ivs:
             logging.debug('iv %s', iv)
-            ans.update(self._intersects1(iv))
+            ans += self._intersects1(iv)
+            logging.debug('ans %s', ans)
         return ans
 
 # --------------------------------------------------------------------------------------
@@ -57,16 +75,18 @@ def ivsofstr(s):
 
 if __name__ == '__main__':
     n, m = map(int, stdin.readline().split())
+    logging.debug('n %s, m %s', n, m)
     genes = DIS2([ivsofstr(stdin.readline()) for _ in range(n)])
     reads = [ivsofstr(stdin.readline()) for _ in range(m)]
 
     ans = [0] * n
     # tmp = [-1] * m
     #
-    tmp = [list(genes.intersects(reads[j])) for j in range(len(reads))]
-    for x in tmp:
-        if len(x) == 1:
-            ans[x[0]] += 1
+    tmp = [genes.intersects(reads[j]) for j in range(len(reads))]
+    logging.debug('tmp %s', tmp)
+    # for x in tmp:
+    #     if len(x) == 1:
+    #         ans[x[0]] += 1
     #         if tmp[j] == -1:
     #             if reads[j].intersects(genes[i]):
     #                 tmp[j] = i
