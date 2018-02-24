33c136d5a7026af1056b47fbe8b8bea6c8da6866
Anton Yabchinskiy <arn@bestmx.net>
2018-02-24 16:18:13 +0300

    2018/f4: GC content based solution

---
+++ 2018/f4.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import logging
+from logging import debug
+from sys import stdin
+from time import time
+
+logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+
+def gc_content(s):
+    return (s.count("G") + s.count("C")) / len(s)
+
+def read_fasta(f):
+    ans = []
+    while True:
+        ident = f.readline().lstrip("> ").rstrip()
+        seq = f.readline().strip().upper()
+        if ident == "" and seq == "":
+            break
+        ans.append((ident, seq))
+    return ans
+
+if __name__ == "__main__":
+    ans = dict()
+    for ident, seq in read_fasta(stdin):
+        # c = round(100 * gc_content(seq))
+        c = round(1000 * gc_content(seq))
+        ans.setdefault(c, set()).add(ident)
+    for c, ident_set in ans.items():
+        for ident in ident_set:
+            print(ident, hash(c))
